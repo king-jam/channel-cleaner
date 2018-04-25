b217dfdf6b76a3de8de2d375bbffa66ce2b91a6a
James King <james.king@dell.com>
2018-04-24 22:19:19 -0400

    Oauth flow...maybe?

--- main.go
+++ main.go
@@ -9,48 +9,64 @@ import (
 	"github.com/nlopes/slack"
 )
 
+var tokenStore map[string]string
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	token := os.Getenv("SLACK_TOKEN")
-	if token == "" {
-		log.Fatal("$SLACK_TOKEN must be set")
+	clientID := os.Getenv("CLIENT_ID")
+	if clientID == "" {
+		log.Fatal("$CLIENT_ID must be set")
 	}
 
-	validationToken := os.Getenv("VALIDATION_TOKEN")
-	if validationToken == "" {
-		log.Fatal("$VALIDATION_TOKEN must be set")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	if clientSecret == "" {
+		log.Fatal("$CLIENT_SECRET must be set")
+	}
+
+	verificationToken := os.Getenv("VERIFICATION_TOKEN")
+	if verificationToken == "" {
+		log.Fatal("$VERIFICATION_TOKEN must be set")
+	}
+
+	redirectURI := os.Getenv("REDIRECT_URI")
+	if redirectURI == "" {
+		log.Fatal("$REDIRECT_URI must be set")
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
 
-	/*
-		token=gIkuvaNzQIHg97ATvDxqgjtO
-		team_id=T0001
-		team_domain=example
-		enterprise_id=E0001
-		enterprise_name=Globular%20Construct%20Inc
-		channel_id=C2147483705
-		channel_name=test
-		user_id=U2147483697
-		user_name=Steve
-		command=/weather
-		text=94070
-		response_url=https://hooks.slack.com/commands/1234/5678
-		trigger_id=13345224609.738474920.8088930838d88f008e0
-	*/
+	router.LoadHTMLFiles("static/add_to_slack.html")
 
-	router.POST("/", func(c *gin.Context) {
+	router.GET("/add", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "add_to_slack.html", nil)
+	})
+
+	router.GET("/auth/redirect", func(c *gin.Context) {
+		code := c.Query("code")
+		response, err := slack.GetOAuthResponse(clientID, clientSecret, code, redirectURI, false)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+		}
+		tokenStore[response.UserID] = response.AccessToken
+		c.Status(http.StatusOK)
+	})
+
+	router.POST("/slashcommand/sbedit", func(c *gin.Context) {
 		slashCommand, err := slack.SlashCommandParse(c.Request)
 		if err != nil {
 			c.Status(http.StatusInternalServerError)
 		}
-		slashCommand.ValidateToken(validationToken)
+		slashCommand.ValidateToken(verificationToken)
 		if err != nil {
+			c.Status(http.StatusInternalServerError)
+		}
+		token, ok := tokenStore[slashCommand.UserID]
+		if !ok {
 			c.Status(http.StatusInternalServerError)
 		}
 		handleIt(token, slashCommand)
